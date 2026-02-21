63fbc4a92b6d295015a29a364ac21a50fc521b71
Darshan Parmar <parmardarshanr1@gmail.com>
2026-02-21 15:01:05 +0530

    Add Some Comments

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-const cookieParser = require('cookie-parser')
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+// const cookieParser = require('cookie-parser')
+// const express = require('express')
+// const app = express()
+// const bcrypt = require('bcrypt')
+// const jwt = require('jsonwebtoken')
 
-const port = 3000
+// const port = 3000
 
-app.use(cookieParser())
+// app.use(cookieParser())
 
-// Encrypt Password 
+// // Encrypt Password 
+// // app.get('/', (req, res) => {
+// //    bcrypt.genSalt(10, function(err, salt) {
+// //     bcrypt.hash("myPlaintextPassword", salt, function(err, hash) {
+// //         console.log(hash)
+// //     });
+// // });
+// // })
+
+// // Compare Password
+// // app.get('/', (req, res) => {
+// //     bcrypt.compare("myPlaintextPassword", "$2b$10$QxT5sOhsrecbIM7DvXmoKe7tZa8ZYeYLxHWx3wkCC9upJyTPUrVrW", function(err, result) {
+// //     console.log(result) // true or false
+// // });
+// // })
+
+// // JWT Sign
 // app.get('/', (req, res) => {
-//    bcrypt.genSalt(10, function(err, salt) {
-//     bcrypt.hash("myPlaintextPassword", salt, function(err, hash) {
-//         console.log(hash)
-//     });
-// });
+//      let token = jwt.sign({email: "user@example.com" }, "secret")
+//      res.cookie("token", token)
+//      res.send("Token has been generated and set in cookie!")
 // })
 
-// Compare Password
-// app.get('/', (req, res) => {
-//     bcrypt.compare("myPlaintextPassword", "$2b$10$QxT5sOhsrecbIM7DvXmoKe7tZa8ZYeYLxHWx3wkCC9upJyTPUrVrW", function(err, result) {
-//     console.log(result) // true or false
-// });
-// })
-
-// JWT Sign
-app.get('/', (req, res) => {
-     let token = jwt.sign({email: "user@example.com" }, "secret")
-     res.cookie("token", token)
-     res.send("Token has been generated and set in cookie!")
-})
-
-// JWT Verify
-app.get('/read', (req, res) => {
-    let data = jwt.verify(req.cookies.token, "secret")
-    console.log( data )
-})
-
-
-// Set cookie 
-// app.get('/', (req, res) => 
-//     res.cookie('name', 'User')
-// .send('Cookie has been set!'))
-
+// // JWT Verify
 // app.get('/read', (req, res) => {
-//     // Access cookies from the request
-//     console.log(req.cookies) 
-//     res.send("read")
+//     let data = jwt.verify(req.cookies.token, "secret")
+//     console.log( data )
 // })
 
-app.listen(3000)
+
+// // Set cookie 
+// // app.get('/', (req, res) => 
+// //     res.cookie('name', 'User')
+// // .send('Cookie has been set!'))
+
+// // app.get('/read', (req, res) => {
+// //     // Access cookies from the request
+// //     console.log(req.cookies) 
+// //     res.send("read")
+// // })
+
+// app.listen(3000)
